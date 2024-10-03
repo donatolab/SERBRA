fixed mahalanobis calculation, fixed distribution compare funciton, issue with cross_entropy

Datasets.py
@@ -410,6 +410,8 @@ class BehaviorDataset(Dataset):
         xticks_pos=None,
         yticks=None,
         ylabel="",
+        save_dir=None,
+        save_plot=False,
     ):
         
         data = data if data is not None else self.binned_data
@@ -427,9 +429,21 @@ class BehaviorDataset(Dataset):
         occupancy[occupancy == 0] = np.nan
         if plot:
             title = f"Occupancy Map {self.metadata['task_id']} {additional_title}"
+            if save_plot:
+                save_dir = save_dir or self.plot_attributes["save_path"].parent
+            else:
+                save_dir = None
+
             if self.key == "stimulus":
                 xticks = xticks or self.plot_attributes["yticks"][1]
                 xticks_pos = xticks_pos or self.plot_attributes["yticks"][0]
+            else:
+                unique_bins = np.array(list(self.category_map.keys()))
+                xticks = np.unique(unique_bins[:, 0])
+                yticks = np.unique(unique_bins[:, 1])
+                xticks_pos = xticks
+                yticks_pos = yticks
+
             # Plot occupancy map
             Vizualizer.plot_heatmap(
                 occupancy,
@@ -438,8 +452,10 @@ class BehaviorDataset(Dataset):
                 xticks=xticks,
                 xticks_pos=xticks_pos,
                 yticks=yticks,
+                yticks_pos=yticks_pos,
                 xlabel=f"{self.key.capitalize()} bins",
                 ylabel=ylabel,
+                save_dir=save_dir,
             )
 
         return occupancy
@@ -550,6 +566,7 @@ class NeuralDataset(Dataset):
         figsize=(6, 5),
         plot: bool = False,
         save_plot: bool = False,
+        save_dir = None,
     ):
         """
         metrics: euclidean, wasserstein, kolmogorov-smirnov, chi2, kullback-leibler, jensen-shannon, energy, mahalanobis, cosine
@@ -580,10 +597,11 @@ class NeuralDataset(Dataset):
                 )
                 title = "Similarity Inside Binned Features"
                 title += f" Embedded" if use_embedding or model is not None else ""
-                additional_title = additional_title
-                xticks = xticks
-                xlabel = xlabel
-                ylabel = ylabel
+                unique_bins = np.array(list(category_map.keys()))
+                xticks = np.unique(unique_bins[:, 0])
+                yticks = np.unique(unique_bins[:, 1])
+                xticks_pos = xticks
+                yticks_pos = yticks
                 to_show_similarities = binned_similarities
             else:
                 group_vectors, bins = group_by_binned_data(
@@ -653,14 +671,14 @@ class NeuralDataset(Dataset):
                 else similarities
             )
             title = title or f"Neural {metric} Similarity {self.metadata['task_id']}"
-            xticks = xticks
-            xticks_pos = xticks_pos
+            yticks = None
+            yticks_pos = None
             xlabel = xlabel or "Frames"
             ylabel = ylabel or "Frames"
 
         if plot:
             if save_plot:
-                save_dir = self.data_dir.joinpath("figures")
+                save_dir = save_dir or self.plot_attributes["save_path"].parent 
             if inside_bin_similarity:
                 title += f" {self.metadata['task_id']}"
                 Vizualizer.plot_heatmap(
@@ -669,7 +687,9 @@ class NeuralDataset(Dataset):
                     figsize=figsize,
                     title=title,
                     xticks=xticks,
+                    yticks=yticks,
                     xticks_pos=xticks_pos,
+                    yticks_pos=yticks_pos,
                     xlabel=xlabel,
                     ylabel=ylabel,
                     save_dir=save_dir,
@@ -692,6 +712,7 @@ class NeuralDataset(Dataset):
             self,
             method="kde",
             model=None,
+            additional_title="",
             idx_to_keep: np.ndarray = None,
             use_embedding: bool = False,
             binned_features: np.ndarray = None,
@@ -705,6 +726,7 @@ class NeuralDataset(Dataset):
             plot_legend=False,
             figsize=(6, 5),
             save_plot: bool = False,
+            save_dir = None,
             ):
         """
         Evaluates the amount of information in the neural data. Based on the distribution of samples labeld by the binned features.
@@ -770,13 +792,31 @@ class NeuralDataset(Dataset):
             title += "embedded" if use_embedding else ""
             title += " filtered" if remove_outliers else ""
 
-            save_dir = self.data_dir.joinpath("figures") if save_plot else None
-            Vizualizer.plot_heatmap(heatmap_data, xlabel="Position Bin X", ylabel="Position Bin Y", title=title, colorbar_label="Entropy", save_dir=save_dir)
+            if save_plot:
+                save_dir = save_dir or self.plot_attributes["save_path"].parent 
+            else:
+                save_dir = None
+            xticks = np.unique(unique_bins[:, 0])
+            yticks = np.unique(unique_bins[:, 1])
+            xticks_pos = xticks
+            yticks_pos = yticks
+            Vizualizer.plot_heatmap(heatmap_data, 
+                                    title=title, 
+                                    additional_title=additional_title, 
+                                    xticks=xticks,
+                                    yticks=yticks,
+                                    xticks_pos=xticks_pos,
+                                    yticks_pos=yticks_pos,
+                                    xlabel="Position Bin X", 
+                                    ylabel="Position Bin Y", 
+                                    colorbar_label="Entropy", 
+                                    save_dir=save_dir)
         return inf_contents
         
     def density(
            self,
             model=None,
+            additional_title="",
             idx_to_keep: np.ndarray = None,
             use_embedding: bool = False,
             binned_features: np.ndarray = None,
@@ -788,7 +828,7 @@ class NeuralDataset(Dataset):
             plot_legend=False,
             use_alpha=False,      
             save_plot: bool = False,
-            save_dir: Path = None,
+            save_dir = None,
     ):
         filtered_neural_data = self.get_data(use_embedding, model, idx_to_keep)
         filtered_binned_features = self.filter_by_idx(
@@ -820,9 +860,13 @@ class NeuralDataset(Dataset):
                 usefull_ids = np.where(group_densities > outlier_threshold)
                 usefull_idx.append(usefull_ids)
 
-        additional_title = f" from and to each Bin {self.metadata['task_id']}"
-        if save_plot:
-            save_dir = save_dir or self.data_dir.joinpath("figures")
+        additional_title += f" Bin {self.metadata['task_id']}"
+
+        if plot is not None:
+            if save_plot:
+                save_dir = save_dir or self.plot_attributes["save_path"].parent
+            else:
+                save_dir = None
         if plot==True or plot=="2d":
             Vizualizer.plot_2d_group_scatter(densities, additional_title=additional_title, plot_legend=plot_legend, use_alpha=use_alpha, filter_outlier=remove_outliers, outlier_threshold=outlier_threshold, save_dir=save_dir)
         elif plot=="3d":

Helper.py
@@ -494,7 +494,8 @@ def compare_distributions(
     points1, points2, metric="cosine", filter_outliers=True, parallel=True, neighbor_distance=None, out_det_method="density"
 ):
     """
-    Compare two distributions using the specified metric.
+    Compare two distributions using the specified metric. 
+    CAUTION: Not all metric calculations are optimized with numba
 
     Parameters:
     - points1: array-like, first distribution
@@ -638,15 +639,19 @@ def compare_distributions(
             v2 = np.mean(points2, axis=0)
         return cosine_similarity(v1, v2)
 
-    elif "overlap":
+    elif metric == "overlap":
         kde1 = gaussian_kde(points1.T)
         kde2 = gaussian_kde(points2.T)
         overlap = normalized_kde_overlap(kde1, kde2)
         return overlap
     
-    elif "cross_entropy":
+    elif metric == "cross_entropy":
         # Cross entropy: H(p, q) = -sum(p * log(q))
-        return entropy(points1, points2)
+        kde1 = gaussian_kde(points1.T)
+        kde2 = gaussian_kde(points2.T)
+        kde1_densities = kde1.evaluate(points1.T)
+        kde2_densities = kde2.evaluate(points1.T)
+        return entropy(kde1_densities, kde2_densities)
 
     else:
         raise ValueError(f"Unsupported metric: {metric}")
@@ -822,10 +827,18 @@ def mahalanobis_distance_between_distributions(points1, points2):
     cov_inv1 = np.linalg.inv(cov_matrix1)
     cov_inv2 = np.linalg.inv(cov_matrix2)
 
-    # Compute the Mahalanobis distance between the means of the dists
-    mean1 = np.mean(points1, axis=0)
-    mean2 = np.mean(points2, axis=0)
-    return mahalanobis_distance(mean1, mean2, cov_inv1 + cov_inv2)
+    # Manually compute the mean along axis=0
+    mean1 = np.zeros(points1.shape[1])
+    mean2 = np.zeros(points2.shape[1])
+    
+    for i in range(points1.shape[1]):
+        mean1[i] = np.sum(points1[:, i]) / points1.shape[0]
+        mean2[i] = np.sum(points2[:, i]) / points2.shape[0]
+
+    # Sum of inverses of the covariance matrices
+    cov_inv_sum = cov_inv1 + cov_inv2
+
+    return mahalanobis_distance(mean1, mean2, cov_inv_sum)
 
 @njit(nopython=True, parallel=True)
 def compute_mahalanobis_distances(points, mean, inv_cov):
@@ -993,7 +1006,6 @@ def get_outlier_mask_numba(points, contamination=0.2, neighbor_distance=None, me
             return np.ones(len(valid_points), dtype=np.bool_)
         
         distances = compute_mahalanobis_distances(valid_points, mean, inv_cov)
-        print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
 
         # Determine threshold based on contamination (percentage of outliers)
         threshold = np.percentile(distances, 100 * (1 - contamination))
@@ -1343,6 +1355,20 @@ def check_correct_metadata(string_or_list, name_parts):
                 )
     return success
 
+def clean_filename(fname):
+    """
+    Clean a filename by replacing special characters with underscores.
+    """
+    nice = fname
+    for char in [" ", ":", ",", ";", "."]:
+        nice = nice.replace(char, "_")
+    nice = nice.replace(">", "bigger")
+    nice = nice.replace("<", "smaller")
+    for special_char in ["<", ">", ":", "\"", "/", "\\", "|", "?", "*"]:
+        nice = nice.replace(special_char, "")
+    for char in ["(", ")", "[", "]", "{", "}"]:
+        nice = nice.replace(special_char, "")
+    return nice
 
 # array
 def encode_categorical(data, categories=None, return_category_map=False):
@@ -2157,7 +2183,7 @@ def profile_function(file_name="profile_output"):
             output_text = profiler.output_text(unicode=True, color=True)
             print(output_text)
             # Save the output to an HTML file
-            with open(f'{file_name}.html', 'w') as f:
+            with open(f'./logs/{file_name}.html', 'w') as f:
                 f.write(profiler.output_html())
             return result
         return wrap_func

Visualizer.py
@@ -1423,14 +1423,7 @@ class Vizualizer:
         plt.tight_layout()  # Ensure subplots fit within figure area
         
         if save:
-            plot_save_path = (
-                str(self.save_dir.joinpath(title + ".png"))
-                .replace(">", "bigger")
-                .replace("<", "smaller")
-            )
-            title = Path(plot_save_path).stem
-            save_dir = Path(plot_save_path).parent
-            Vizualizer.save_plot(save_dir, title, "pdf" if as_pdf else "png")
+            Vizualizer.save_plot(self.save_dir, title, "pdf" if as_pdf else "png")
 
         plt.show()
         plt.close()
@@ -2709,7 +2702,7 @@ class Vizualizer:
 
         tick_steps = make_list_ifnot(tick_steps)
         for i, tick_step in enumerate(tick_steps):
-            tick_steps[i] = 1 if tick_step is 0 else tick_step
+            tick_steps[i] = 1 if tick_step == 0 else tick_step
         if len(tick_steps) != len(max_bins):
             tick_steps = [tick_steps[0]] * len(max_bins)
         
@@ -2737,23 +2730,23 @@ class Vizualizer:
                     1,
                     figsize=(figsize[0], figsize[1]),
                 )
-                fig.suptitle(title, fontsize=max_bins * max_bins, y=1.01)
-                fig.supxlabel(supxlabel, fontsize=max_bins * max_bins, x=0.5, y=-0.03)
+                fig.suptitle(title, fontsize=figsize[0]+figsize[1], y=1.01)
+                fig.supxlabel(supxlabel, fontsize=figsize[0]+figsize[1], x=0.5, y=-0.03)
                 fig.align_xlabels()
-                fig.supylabel(supylabel, fontsize=max_bins * max_bins, x=-0.02, y=0.5)
+                fig.supylabel(supylabel, fontsize=figsize[0]*figsize[1], x=-0.02, y=0.5)
             elif max_bins.ndim == 1:
                 fig, axes = plt.subplots(
                     int(max_bins[0]),
                     int(max_bins[1]),
                     figsize=(max_bins[0] * figsize[0], max_bins[1] * figsize[1]),
                 )
-                fig.suptitle(title, fontsize=max_bins[0] * max_bins[1] / 2, y=1.01)
+                fig.suptitle(title, fontsize=figsize[0]*figsize[1], y=1.01)
                 fig.supxlabel(
-                    supxlabel, fontsize=max_bins[0] * max_bins[1] / 2, x=0.5, y=-0.03
+                    supxlabel, fontsize=figsize[0]*figsize[1], x=0.5, y=-0.03
                 )
                 fig.align_xlabels()
                 fig.supylabel(
-                    supylabel, fontsize=max_bins[0] * max_bins[1] / 2, x=-0.02, y=0.5
+                    supylabel, fontsize=figsize[0]*figsize[1], x=-0.02, y=0.5
                 )
             fig.tight_layout()
 
@@ -2848,7 +2841,7 @@ class Vizualizer:
                     fig=fig,
                     labels=labels,
                     label_name=colorbar_label,
-                    label_size=max(max_bins) * 2,
+                    label_size=figsize[0] * 2,
                     cmap=cmap,
                     move_right=1,
                 )
@@ -2989,6 +2982,8 @@ class Vizualizer:
     @staticmethod
     def save_plot(save_dir=None, title="NONAME_DEFINED", format="pdf"):
         if save_dir:
+            # clean title
+            title = clean_filename(title)
             save_path = os.path.join(save_dir, f"{title}.{format}")
             plt.savefig(save_path, 
                         dpi=300, 
@@ -3065,7 +3060,7 @@ class Vizualizer:
         title = '3D Scatter'
         title += f" {additional_title}" if additional_title else ""
         title += f" {specific_group}" if specific_group else ""
-        title += f" no alpha" if not use_alpha else ""
+        title += f" w/o alpha" if not use_alpha else ""
 
         # Define unique colors for each group
         group_colors = plt.cm.get_cmap(cmap, len(gropu_data))  # Get a colormap with distinct colors
@@ -3156,8 +3151,8 @@ class Vizualizer:
         title = 'Point Distributions in 2D'
         title += f" {additional_title}" if additional_title else ""
         title += f" pca" if use_pca else ""
-        title += f" no alpha" if not use_alpha else ""
-        title += f" no outlier" if filter_outlier else ""
+        title += f" w/o alpha" if not use_alpha else ""
+        title += f" w/o outlier" if filter_outlier else ""
 
         # Determine the number of subplots
         unique_bins = np.unique(list(group_data.keys()), axis=0)
@@ -3217,10 +3212,10 @@ class Vizualizer:
 
         if filter_outlier:
             title += f" {c_outliers/c_samples:.2%} outliers"
-        fig.suptitle(title, fontsize=figsize[1], y=1.01)
-        fig.supxlabel(supxlabel, fontsize=figsize[1], x=0.5, y=-0.03)
+        fig.suptitle(title, fontsize=figsize[0]+figsize[1], y=1.01)
+        fig.supxlabel(supxlabel, fontsize=figsize[0]+figsize[1], x=0.5, y=-0.03)
         fig.align_xlabels()
-        fig.supylabel(supylabel, fontsize=figsize[1], x=-0.02, y=0.5)
+        fig.supylabel(supylabel, fontsize=figsize[0]+figsize[1], x=-0.02, y=0.5)
 
         if plot_legend:
             plt.legend()